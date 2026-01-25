a40d6503438c0588fd1dd7f0bb030b34ab379095
Kaori <ichika200010@gmail.com>
2026-01-26 08:51:16 +1300

    feat(auth): protect API with Cognito JWT authorizer

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -7,6 +7,7 @@ import { RemovalPolicy } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigwv2 from 'aws-cdk-lib/aws-apigatewayv2';
 import * as integrations from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import * as authorizers from 'aws-cdk-lib/aws-apigatewayv2-authorizers';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as path from 'path';
 
@@ -20,7 +21,6 @@ export class CdkStack extends cdk.Stack {
     // DynamoDB
     const itemsTable = new dynamodb.Table(this, 'ItemsTable', {
       tableName: `Items-${stage}`,
-      // pk: USER#{userId}, sk: ITEM#{itemId}
       partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
@@ -47,64 +47,62 @@ export class CdkStack extends cdk.Stack {
     // Cognito Client
     const userPoolClient = new cognito.UserPoolClient(this, 'UserPoolClient', {
       userPool,
-      authFlows: {
-        userPassword: true,
-        userSrp: true,
-      },
+      authFlows: { userPassword: true, userSrp: true },
     });
 
+    // Lambdas
+    const commonEnv = { ITEMS_TABLE_NAME: itemsTable.tableName };
 
+    const createItemFn = new NodejsFunction(this, 'CreateItemFn', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, '../../apps/a-api/src/handlers/createItem.ts'),
+      handler: 'handler',
+      environment: commonEnv,
+    });
 
+    const listItemsFn = new NodejsFunction(this, 'ListItemsFn', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, '../../apps/a-api/src/handlers/listItems.ts'),
+      handler: 'handler',
+      environment: commonEnv,
+    });
 
-    const commonEnv = {
-  ITEMS_TABLE_NAME: itemsTable.tableName,
-};
+    // Grant DynamoDB permissions to Lambdas
+    itemsTable.grantReadWriteData(createItemFn);
+    itemsTable.grantReadData(listItemsFn);
 
-const createItemFn = new NodejsFunction(this, 'CreateItemFn', {
-  runtime: lambda.Runtime.NODEJS_20_X,
-  entry: path.join(__dirname, '../../apps/a-api/src/handlers/createItem.ts'),
-  handler: 'handler',
-  environment: commonEnv,
-});
+    // HTTP API
+    const httpApi = new apigwv2.HttpApi(this, 'HttpApi', {
+      apiName: `project-a-http-api-${stage}`,
+    });
 
-const listItemsFn = new NodejsFunction(this, 'ListItemsFn', {
-  runtime: lambda.Runtime.NODEJS_20_X,
-  entry: path.join(__dirname, '../../apps/a-api/src/handlers/listItems.ts'),
-  handler: 'handler',
-  environment: commonEnv,
-});
+    // JWT Authorizer (Cognito)
+    const issuer = `https://cognito-idp.${this.region}.amazonaws.com/${userPool.userPoolId}`;
+    const jwtAuthorizer = new authorizers.HttpJwtAuthorizer('JwtAuthorizer', issuer, {
+      jwtAudience: [userPoolClient.userPoolClientId],
+    });
 
-// Grant DynamoDB permissions to Lambdas
-itemsTable.grantReadWriteData(createItemFn);
-itemsTable.grantReadData(listItemsFn);
+    // Routes (protected)
+    httpApi.addRoutes({
+      path: '/items',
+      methods: [apigwv2.HttpMethod.POST],
+      integration: new integrations.HttpLambdaIntegration('CreateItemIntegration', createItemFn),
+      authorizer: jwtAuthorizer,
+    });
 
-
-const httpApi = new apigwv2.HttpApi(this, 'HttpApi', {
-  apiName: `project-a-http-api-${stage}`,
-});
-
-httpApi.addRoutes({
-  path: '/items',
-  methods: [apigwv2.HttpMethod.POST],
-  integration: new integrations.HttpLambdaIntegration('CreateItemIntegration', createItemFn),
-});
-
-httpApi.addRoutes({
-  path: '/items',
-  methods: [apigwv2.HttpMethod.GET],
-  integration: new integrations.HttpLambdaIntegration('ListItemsIntegration', listItemsFn),
-});
-   new cdk.CfnOutput(this, 'HttpApiUrl', { value: httpApi.url! });
-
-
+    httpApi.addRoutes({
+      path: '/items',
+      methods: [apigwv2.HttpMethod.GET],
+      integration: new integrations.HttpLambdaIntegration('ListItemsIntegration', listItemsFn),
+      authorizer: jwtAuthorizer,
+    });
 
     // Outputs
+    new cdk.CfnOutput(this, 'HttpApiUrl', { value: httpApi.url! });
     new cdk.CfnOutput(this, 'Stage', { value: stage });
     new cdk.CfnOutput(this, 'ItemsTableName', { value: itemsTable.tableName });
+    new cdk.CfnOutput(this, 'ItemsTableArn', { value: itemsTable.tableArn });
     new cdk.CfnOutput(this, 'UserPoolId', { value: userPool.userPoolId });
     new cdk.CfnOutput(this, 'UserPoolClientId', { value: userPoolClient.userPoolClientId });
-    new cdk.CfnOutput(this, 'ItemsTableArn', { value: itemsTable.tableArn });
- 
-
   }
 }
